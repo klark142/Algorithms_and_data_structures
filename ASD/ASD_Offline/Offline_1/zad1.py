273e4d67cc4bd2b6b5c69cfff7802b091281a9fa
Jakub Karoń <jakubkaron@MacBook-Air.local>
2023-03-12 12:54:11 +0100

    Initial Commit

--- ASD/ASD_Offline/Offline_1/zad1.py
+++ ASD/ASD_Offline/Offline_1/zad1.py
@@ -1,11 +1,18 @@
 from zad1testy import runtests
+# Jakub Karoń
+# Zadanie rozwiązane za pomocą algorytmu Manachera, który wykorzystując własności symetrii palindromu
+# pozwala na redukcję złożoności obliczeniowej do O(n). Dla każdej litery w wejściowym napisie sprawdzamy,
+# czy litera ta nie jest środkiem podpalindromu zawierającego się w innym, dłuższym palindromie.
+# W ten sposób możemy zmniejszyć liczbę porównań poprzez ustalenie początkowej długości palindromu.
+# Promienie palindromów dla każdej litery przechowywujemy w tablicy pomocniczej.
 
 
 def ceasar(s):
-    left = 0
-    right = float("-inf")
+    left = 0 # lewa granica dłuższego palindromu
+    right = float("-inf") # prawa granica dłuższego palindromu
     n = len(s)
-    p_lengths = [0] * n  # tablica, której wartość p_lengths[i] odpowiada maksymalnej długości palindromu o środku s[i]
+    p_lengths = [0] * n  # tablica, której wartość p_lengths[i] odpowiada maksymalnemu promieniowi palindromu o
+    # środku s[i]
     for i in range(n):
         # zmniejszenie liczby porównań poprzez sprawdzanie, czy rozważany palindrom zawiera się w innym palindromie i
         # można przypisać mu już początkową długość
@@ -18,11 +25,14 @@ def ceasar(s):
         curr_left = i - p_lengths[i] - 1
         curr_right = i + p_lengths[i] + 1
 
+        # po ustaleniu początkowego promienia próbujemy maksymalnie rozszerzyć rozważany palindrom
         while curr_left >= 0 and curr_right < n and s[curr_left] == s[curr_right]:
             p_lengths[i] += 1
             curr_left -= 1
             curr_right += 1
 
+        # sprawdzamy, czy obecnie rozważony palindrom nie wyszedł poza zakres najdłuższego palindromu
+        # jeśli tak, to aktualizujemy obecnie najdłuższy palindrom
         if i + p_lengths[i] > right:
             left = i - p_lengths[i]
             right = i + p_lengths[i]
