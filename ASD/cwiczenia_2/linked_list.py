4d5a3a3a94bdf4a17a8e1545707167ba10a7f81f
jakubkaron <kkaron0313@gmail.com>
2023-03-20 14:20:38 +0100

    added cwiczenia 3

--- ASD/cwiczenia_2/linked_list.py
+++ ASD/cwiczenia_2/linked_list.py
@@ -6,6 +6,7 @@ class Node:
         self.val = val
         self.next = None
 
+
 def insert_node(p, new_element):
     g = new_element
     while p.next is not None and p.next.val < new_element.val:
@@ -13,6 +14,7 @@ def insert_node(p, new_element):
     p.next = new_element.next
     new_element.next = p
     return g
+
 
 def insertion_sort_linked_list(p):
     head = p
